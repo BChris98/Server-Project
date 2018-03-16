813b6fc67fbd1944ce8a76cdf5b1f5c0bb6d8f11
Bournousouzis Christos <36258778+BChris98@users.noreply.github.com>
2018-03-16 16:05:08 +0100

    Upload data's

---
+++ clientserv.py
@@ -0,0 +1,112 @@
+import socket
+import sys
+import threading
+import re
+
+class Server:
+    def __init__(self,SERVERADDRESS):
+        self.__adr = SERVERADDRESS
+        self.__s = socket.socket()
+        self.__s.bind(self.__adr)
+        self.__client = [socket.gethostname()]
+        with open ("pseudo.txt","w") as file:
+            file.write ("admin")
+            file.write("\n")
+            
+    def run(self):
+        self.__s.listen()
+        while True:
+
+            client, addr = self.__s.accept()
+            with open ("pseudo.txt","r") as file:
+                pseudos = file.readlines ()
+
+            if len(pseudos) > len(self.__client):
+                self.__client.append (addr[0])
+
+            try:
+                recu = self._receive(client).decode()
+                p = re.compile ("(?P<pseudo>.+)(?P<residu>\\n)")
+                if recu == "clientlist":
+                    liste_client = []
+                    for i in range (0,len(pseudos)):
+                        m = p.match (pseudos[i])
+                        liste_client.append ((self.__client[i],m.group("pseudo")))
+                    print ("Les utilisateurs connectés sont :")
+                    for i in liste_client:
+                        print ("{} : {}".format(i[1],i[0]))
+                else:
+                    place = self.__client.index(addr[0])
+                    m2 = p.match (pseudos[place])
+                    print("{} : {}".format(m2.group("pseudo"),recu))
+                client.close()
+            except OSError:
+                print('Erreur lors de la réception du message.')
+
+    def _receive(self, client):
+        chunks = []
+        finished = False
+        while not finished:
+            data = client.recv(1024)
+            chunks.append(data)
+            finished = data == b''
+        return b''.join(chunks)
+
+
+class Client:
+    def __init__(self, SERVERADDRESS,pseudo):
+        self.__adr = SERVERADDRESS
+        self.__pseudo = pseudo
+        with open ("pseudo.txt","a") as file:
+            file.write (self.__pseudo)
+            file.write("\n")
+
+    def run(self):
+        handlers = {
+            '/send': self._send,
+            '/client': self._client
+        }
+        self.__running = True
+        self.__address = None
+        while self.__running:
+            line = sys.stdin.readline().rstrip() + ' '
+            # Extract the command and the param
+            command = line[:line.index(' ')]
+            param = line[line.index(' ') + 1:].rstrip()
+            # Call the command handler
+            if command in handlers:
+                try:
+                    handlers[command]() if param == '' else handlers[command](param)
+                except:
+                    print("Erreur lors de l'exécution de la commande.")
+            else:
+                print('Command inconnue:', command)
+    
+    def _send(self,message):
+        try:
+            self.__s = socket.socket()
+            self.__s.connect(self.__adr)
+            totalsent = 0
+            msg = message
+            try:
+                while totalsent < len(msg):
+                    sent = self.__s.send(msg[totalsent:].encode())
+                    totalsent += sent
+            except OSError:
+                print("Erreur lors de l'envoi du message.")
+            self.__s.close()
+        
+        except OSError:
+            print('Serveur introuvable, connexion impossible.')
+    
+    def _client (self):
+        self._send ("clientlist")
+        
+    def _receive(self, client):
+        chunks = []
+        finished = False
+        while not finished:
+            data = client.recv(1024)
+            chunks.append(data)
+            finished = data == b''
+        return b''.join(chunks)
---
+++ main.py
@@ -0,0 +1,62 @@
+from peertopeer import *
+from clientserv import *
+import socket
+import re
+
+class Intro :
+    def __init__ (self):
+
+        print ("Souhaitez vous vous connectez à un serveur(1), à une machine(2) ou héberger un serveur(3)")
+        choix = int(input())
+        liste = [1,2,3]
+        if choix not in liste:
+            print ("Erreur dans l'exécution de la commande")
+            self.__init__()
+
+        if choix == 1:
+            self.server()
+        elif choix == 2:
+            self.machine()
+        else:
+            self.heberge()
+
+    def server (self):
+
+        print ("Identifiez-vous avec un pseudo")
+        pseudo = str(input())
+        if pseudo == '':
+            print ("Erreur dans l'encodage du pseudo")
+            self.server ()
+
+        print ("Quel est l'adresse ip du serveur sur lequel vous souhaitez vous connectez ?")
+        ip = str(input())
+        pattern = r"(\d{1,3}.){3}\d{1,3}"
+        p = re.compile (pattern)
+        if p.match (ip) is None:
+            print ("Erreur dans l'encodage de l'adresse ip")
+            self.server()
+        try:
+            print ("Et le port de connection ?")
+            port = int(input())
+        except:
+            print ("Erreur dans l'encodage du port")
+            self.server()
+            
+        adress = (ip,port)
+        Client(adress,pseudo).run()
+    
+    def machine (self):
+
+        print("Sous quel port souhaitez vous écouter ?")
+        port = int(input())
+        Chat(socket.gethostname(),port).run()
+
+    def heberge (self):
+
+        print ("Sur quel port souhaitez vous héberger votre serveur?")
+        port = int(input())
+        adress = (socket.gethostname(), port)
+        Server(adress).run()
+
+if __name__ == '__main__':
+    Intro()
