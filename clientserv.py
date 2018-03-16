834be95581966eb5d0295f995e8b34e9c18557a7
Bournousouzis Christos <16219@ecam.be>
2018-03-16 16:19:56 +0100

    Blindage

--- clientserv.py
+++ clientserv.py
@@ -9,17 +9,24 @@ class Server:
         self.__s = socket.socket()
         self.__s.bind(self.__adr)
         self.__client = [socket.gethostname()]
-        with open ("pseudo.txt","w") as file:
-            file.write ("admin")
-            file.write("\n")
+        try:
+            with open ("pseudo.txt","w") as file:
+                file.write ("admin")
+                file.write("\n")
+        
+        except:
+            print ("Erreur dans l'ouverture de la base de données")
             
     def run(self):
         self.__s.listen()
         while True:
 
             client, addr = self.__s.accept()
-            with open ("pseudo.txt","r") as file:
-                pseudos = file.readlines ()
+            try:
+                with open ("pseudo.txt","r") as file:
+                    pseudos = file.readlines ()
+            except:
+                print ("Erreur dans l'ouverture de la base de données")
 
             if len(pseudos) > len(self.__client):
                 self.__client.append (addr[0])
@@ -27,19 +34,25 @@ class Server:
             try:
                 recu = self._receive(client).decode()
                 p = re.compile ("(?P<pseudo>.+)(?P<residu>\\n)")
+
                 if recu == "clientlist":
                     liste_client = []
+
                     for i in range (0,len(pseudos)):
                         m = p.match (pseudos[i])
                         liste_client.append ((self.__client[i],m.group("pseudo")))
+
                     print ("Les utilisateurs connectés sont :")
                     for i in liste_client:
                         print ("{} : {}".format(i[1],i[0]))
+
                 else:
                     place = self.__client.index(addr[0])
                     m2 = p.match (pseudos[place])
                     print("{} : {}".format(m2.group("pseudo"),recu))
+
                 client.close()
+
             except OSError:
                 print('Erreur lors de la réception du message.')
 
@@ -57,9 +70,12 @@ class Client:
     def __init__(self, SERVERADDRESS,pseudo):
         self.__adr = SERVERADDRESS
         self.__pseudo = pseudo
-        with open ("pseudo.txt","a") as file:
-            file.write (self.__pseudo)
-            file.write("\n")
+        try:
+            with open ("pseudo.txt","a") as file:
+                file.write (self.__pseudo)
+                file.write("\n")
+        except:
+            print ("Erreur dans l'ouverture de la base de données")
 
     def run(self):
         handlers = {
